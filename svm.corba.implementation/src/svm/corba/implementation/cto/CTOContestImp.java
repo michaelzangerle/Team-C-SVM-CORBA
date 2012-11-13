b9db7db01894883b12836402255e6025774b3977
thomasgaida <thomas.gaida@students.fhv.at>
2012-11-13 15:27:39 +0100

    vcs.xml push

---
+++ svm.corba.implementation/src/svm/corba/implementation/cto/CTOContestImp.java
@@ -0,0 +1,69 @@
+package svm.corba.implementation.cto;
+
+import svm.corba.abstraction.cto.auth.CTOAuthPOA;
+import svm.corba.abstraction.cto.contest.CTOContestPOA;
+import svm.logic.abstraction.transferobjects.ITransferContest;
+
+import java.util.Date;
+
+/**
+ * Projectteam : Team C
+ * Date: 13.11.12
+ */
+public class CTOContestImp extends CTOContestPOA implements ICTO<ITransferContest> {
+
+    private ITransferContest location;
+    private Date creation;
+    private int id;
+
+    public CTOContestImp(ITransferContest contest) {
+        this.location = contest;
+        this.creation = new Date();
+    }
+
+
+    @Override
+    public String name() {
+        location.getName();
+    }
+
+    @Override
+    public long startDate() {
+        return location.getStart().getTime();
+    }
+
+    @Override
+    public long endDate() {
+        return location.getEnd().getTime();
+    }
+
+    @Override
+    public float fee() {
+        return  location.getFee();
+    }
+
+    @Override
+    public svm.corba.abstraction.cto.contactDetails.CTOContactDetails contactDetails() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void setCTOid(int id) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public int CTOid() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public ITransferContest getTransferObject() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public Date getCreation() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
