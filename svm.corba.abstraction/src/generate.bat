4b447cef58c4cbd1d2a1132505f29991634f2ba0
Michael Zangerle <michael.zangerle@gmail.com>
2012-11-13 14:55:34 +0100

    idl files

--- svm.corba.abstraction/src/generate.bat
+++ svm.corba.abstraction/src/generate.bat
@@ -10,11 +10,19 @@ rmdir /s /q svm
 echo -
 echo -- Generate Exceptions
 idlj -fall exceptions/RegisterException.idl
+echo -- -- CTORegisterException
 idlj -fall exceptions/ControllerNotStartedException.idl
+echo -- -- CTOControllerNotStartedException
 idlj -fall exceptions/UnknownException.idl
+echo -- -- CTOUnknownException
 idlj -fall exceptions/AuthNotFoundException.idl
+echo -- -- CTOAuthNotFoundException
 idlj -fall exceptions/AttributeException.idl
+echo -- -- CTOAttributeException
 idlj -fall exceptions/NotAllowException.idl
+echo -- -- CTONotAllowException
+idlj -fall exceptions/LogicException.idl
+echo -- -- CTOLogicException
 
 echo -
 echo -- Generate CTO's
@@ -22,10 +30,14 @@ echo -- -- CTO
 idlj -fall cto/CTO.idl
 echo -- -- CTOAuth
 idlj -fall cto/CTOAuth.idl
+echo -- -- CTOContactDetails
+idlj -fall cto/CTOContactDetails.idl
 echo -- -- CTOContest
 idlj -fall cto/CTOContest.idl
 echo -- -- CTOLocation
 idlj -fall cto/CTOLocation.idl
+echo -- -- CTOMatchType
+idlj -fall cto/CTOMatchType.idl
 echo -- -- CTOMatch
 idlj -fall cto/CTOMatch.idl
 echo -- -- CTOMember
@@ -52,3 +64,5 @@ echo -
 echo -----------------------------------
 echo --      End of Generation        --
 echo -----------------------------------
+
+pause
