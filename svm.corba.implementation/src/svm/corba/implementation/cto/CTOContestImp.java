2bb6c1a77ab10d7c53fb909509bf3ab08fc23651
thomasgaida <thomas.gaida@students.fhv.at>
2012-11-13 15:40:06 +0100

    ctoContest

--- svm.corba.implementation/src/svm/corba/implementation/cto/CTOContestImp.java
+++ svm.corba.implementation/src/svm/corba/implementation/cto/CTOContestImp.java
@@ -1,8 +1,7 @@
 package svm.corba.implementation.cto;
 
-import svm.corba.Main;
-import svm.corba.abstraction.cto.auth.CTOAuthPOA;
 import svm.corba.abstraction.cto.contest.CTOContestPOA;
+import svm.corba.abstraction.exceptions.LogicException;
 import svm.logic.abstraction.exception.IllegalGetInstanceException;
 import svm.logic.abstraction.transferobjects.ITransferContest;
 
@@ -14,61 +13,62 @@ import java.util.Date;
  */
 public class CTOContestImp extends CTOContestPOA implements ICTO<ITransferContest> {
 
-    private ITransferContest location;
+    private ITransferContest contest;
     private Date creation;
     private int id;
 
     public CTOContestImp(ITransferContest contest) {
-        this.location = contest;
+        this.contest = contest;
         this.creation = new Date();
     }
 
 
     @Override
     public String name() {
-        location.getName();
+        return contest.getName();
     }
 
     @Override
     public long startDate() {
-        return location.getStart().getTime();
+        return contest.getStart().getTime();
     }
 
     @Override
     public long endDate() {
-        return location.getEnd().getTime();
+        return contest.getEnd().getTime();
     }
 
     @Override
     public float fee() {
-        return  location.getFee();
+        return contest.getFee();
     }
 
     @Override
-    public svm.corba.abstraction.cto.contactDetails.CTOContactDetails contactDetails() {
+    public svm.corba.abstraction.cto.contactDetails.CTOContactDetails contactDetails() throws LogicException {
         try {
-            return new CTOContactDetailsImpl(location.getContactDetails());
+            return new CTOContactDetailsImpl(contest.getContactDetails())._this();
         } catch (IllegalGetInstanceException e) {
+            throw new LogicException(e.getMessage());
         }
     }
 
     @Override
     public void setCTOid(int id) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        this.id = id;
     }
 
     @Override
     public int CTOid() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return id;
     }
 
     @Override
     public ITransferContest getTransferObject() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return contest;
     }
 
     @Override
     public Date getCreation() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return creation;
     }
 }
