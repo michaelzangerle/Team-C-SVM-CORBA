3cfc3393b0ece32d684f7683bdfcadcf497b301c
thomasgaida <thomas.gaida@students.fhv.at>
2012-11-13 15:43:39 +0100

    ctoMatchTyp

---
+++ svm.corba.implementation/src/svm/corba/implementation/cto/CTOMatchTyp.java
@@ -0,0 +1,47 @@
+package svm.corba.implementation.cto;
+
+import svm.corba.abstraction.cto.matchType.CTOMatchTypePOA;
+import svm.logic.abstraction.transferobjects.ITransferMatchType;
+
+import java.util.Date;
+
+/**
+ * Projectteam : Team C
+ * Date: 13.11.12
+ */
+public class CTOMatchTyp extends CTOMatchTypePOA implements ICTO<ITransferMatchType> {
+
+    private ITransferMatchType matchTyp;
+    private Date creation;
+    private int id;
+
+    public CTOMatchTyp(ITransferMatchType matchTyp) {
+        this.matchTyp = matchTyp;
+        this.creation = new Date();
+    }
+
+    @Override
+    public String name() {
+      return matchTyp.getName();
+    }
+
+    @Override
+    public void setCTOid(int id) {
+       this.id=id;
+    }
+
+    @Override
+    public int CTOid() {
+        return id;
+    }
+
+    @Override
+    public ITransferMatchType getTransferObject() {
+        return matchTyp;
+    }
+
+    @Override
+    public Date getCreation() {
+        return creation;
+    }
+}
